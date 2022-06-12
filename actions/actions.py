32d6fcbee34de9dc4ad29e6f6c758b42ceeac046
Pranshu Bansal <pranshubansal15@gmail.com>
2022-06-12 14:43:12 +0530

    Restart conversation functionality added

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,18 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import Restarted
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionRestart(Action):
+    def name(self) -> Text:
+        return "action_restart"
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [Restarted()]
 
 # This is a simple example for a custom action which utters "Hello World!"
 
